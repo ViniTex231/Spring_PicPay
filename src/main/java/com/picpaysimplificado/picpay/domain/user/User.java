c49d1255e2b6dc42f36afb11ed6a4856cbcd75ce
ViniTex231 <viniteixeira2828@gmail.com>
2024-05-07 13:47:19 -0300

    feat: create User entity

---
+++ src/main/java/com/picpaysimplificado/picpay/domain/user/User.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.picpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera de forma sequencial
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING) // Diz que meu User será um dos dois tipos da classe
+    private UserType userType;
+}
