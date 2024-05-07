bab62e18439e465bd98c14d50c853006ed7d8a07
ViniTex231 <viniteixeira2828@gmail.com>
2024-05-07 13:55:51 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/picpaysimplificado/picpay/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.picpay.repositories;
+
+import com.picpaysimplificado.picpay.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
